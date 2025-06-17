958c3b37e57cbeffcd0a0206dcd1d0cb10f34693
alexborodach30 <sagopien04@gmail.com>
2025-06-17 21:26:30 +0300

    Urls refactoring.

--- food_delivery/food_delivery/urls.py
+++ food_delivery/food_delivery/urls.py
@@ -15,14 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from auth_app.views import RegistrationView, index, LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', RegistrationView.as_view(), name='register'),
-    path('', index, name='index'),
-    path('login/', LoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(), name="logout"),
+    path('', include("auth_app.urls"))
 ]
